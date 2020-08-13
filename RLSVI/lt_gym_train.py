65700ecf2598dc4fdca9028314a95610682bbc4d
Oren Peer <oren.peer2@gmail.com>
2020-08-13 17:09:43 +0300

    Hockey.. not yet working...

--- RLSVI/lt_gym_train.py
+++ RLSVI/lt_gym_train.py
@@ -1,61 +1,63 @@
 import gym
 import pickle
-import Image
+# import Image
 import numpy as np
-import rlsvi
-import tensorflow as tf
-
+from RLSVI.rlsvi import *
+# import tensorflow as tf
+import cv2
 
 # Extract Y channel from RGB, resize to 84x84 and return
-def toNatureDQNFormat(frame) :
-	return np.array(Image.fromarray(frame).convert('YCbCr').resize((84,84),Image.BILINEAR))[:,:,0]/np.float32(255.0)
+# def toNatureDQNFormat(frame):
+#     return np.array(Image.fromarray(frame).convert('YCbCr').resize((84, 84), Image.BILINEAR))[:, :, 0] / np.float32(255.0)
 
-
-def RLVSI_preprocess(frame) :
-	colframe = np.array(Image.fromarray(frame).resize((42,42),Image.BILINEAR))/np.float32(255.0)
-	return np.concatenate([colframe.flatten(),[1]])
+#
+def RLVSI_preprocess(frame):
+    # colframe = np.array(np.resize(frame, (42, 42)))
+    colframe = cv2.resize(frame,  (42, 42), interpolation=cv2.INTER_AREA) / 255.0
+    return np.concatenate([colframe.flatten(), [1]])
 
 
 import gym
+
 env = gym.make('IceHockey-v0')
 
 num_episodes = 500
 num_steps = 15000
 
-
 na = env.action_space.n
-ns = 42*42*3+1
+ns = 42 * 42 * 3 + 1
 
-print na,ns
+print(f'number of episodes: {na}, number of steps: {ns}')
 
-sigma=1.0
-lmb=1.0
-gamma=0.99
+sigma = 1.0
+lmb = 1.0
+gamma = 0.99
 
-R = rlsvi.rlsvi(ns,na,sigma,lmb)
+R = rlsvi(ns, na, sigma, lmb)
 
-print 'here'
-
-sess = tf.InteractiveSession()
-try:
-    sess.run(tf.global_variables_initializer())
-except AttributeError:
-    sess.run(tf.initialize_all_variables())
+# sess = tf.InteractiveSession()
+# try:
+#     sess.run(tf.global_variables_initializer())
+#
+# except AttributeError:
+#     sess.run(tf.initialize_all_variables())
 
 for episode in range(num_episodes):
-	obs = env.reset()
-	obs_pp = RLVSI_preprocess(obs)
-	for time_step in range(num_steps):
-		if time_step % 1000 == 0 :
-			print 'Episode %d Time step %d'%(episode+1,time_step+1)
-		env.render()
-		action = R.choose_action(obs_pp)
-		next_obs, reward, done, info = env.step(action)
-		next_obs_pp = RLVSI_preprocess(next_obs)
-		R.add_data(obs_pp,action,reward,next_obs_pp,done)
-		obs_pp=next_obs_pp
-		if reward!=0 :
-			print reward
-		if done:
-			break
-	R.new_episode(sess)
+    obs = env.reset()
+    obs_pp = RLVSI_preprocess(obs)
+    # obs_pp = obs
+    for time_step in range(num_steps):
+        if time_step % 1000 == 0:
+            print(f'Episode {episode}, Time step {time_step}')
+        # env.render()
+        action = R.choose_action(obs_pp)
+        next_obs, reward, done, info = env.step(action)
+        next_obs_pp = RLVSI_preprocess(next_obs)
+        # next_obs_pp = next_obs
+        R.add_data(obs_pp, action, reward, next_obs_pp, done)
+        obs_pp = next_obs_pp
+        if reward != 0:
+            print(reward)
+        if done:
+            break
+    R.new_episode()
--- RLSVI/rlsvi.py
+++ RLSVI/rlsvi.py
@@ -1,98 +1,99 @@
-import tensorflow as tf
+# import tensorflow as tf
 import numpy as np
 import numpy.random as rn
 
 # Choosing appropriate matrix multiplication function
-if tf.__version__ == '0.10.0':
-    mmul = tf.mul
-else:
-    mmul = tf.matmul
-
-mu = tf.placeholder(tf.float32, [None])
-covmat = tf.placeholder(tf.float32, [None, None])
-eps = tf.placeholder(tf.float32, [None])
-mvn_transform_op = tf.reshape(mmul(covmat, tf.reshape(eps, [-1, 1])), [-1]) + mu
-
-
-def mvn_draw(sess, m, s):
-    return sess.run(mvn_transform_op, feed_dict={mu: m, covmat: s, eps: rn.normal(0, 1, m.size)})
+# if tf.__version__ == '0.10.0':
+#     mmul = tf.mul
+# else:
+#     mmul = tf.matmul
+#
+# mu = tf.placeholder(tf.float32, [None])
+# covmat = tf.placeholder(tf.float32, [None, None])
+# eps = tf.placeholder(tf.float32, [None])
+# mvn_transform_op = tf.reshape(mmul(covmat, tf.reshape(eps, [-1, 1])), [-1]) + mu
 
 
-class bayesian_regressor(object):
+# def mvn_draw(sess, m, s):
+#     return sess.run(mvn_transform_op, feed_dict={mu: m, covmat: s, eps: rn.normal(0, 1, m.size)})
+
+
+class bayesian_regressor:
     # Fit a bayesian least squares regression model with a Gaussian prior
     # and get the estimated coefficients and the covariance
-    def __init__(o, k, sigma=1.0, lmb=1.0):
-        o.X = []
-        o.y = []
-        o.k = k
-        o.sigma = sigma
-        o.sigmasq = sigma * sigma
-        o.lmb = lmb
-        o.theta_est = np.zeros([o.k], dtype=np.float32)
-        o.cov_est = o.sigmasq * np.eye(o.k, dtype=np.float32)
-        o.theta_sample = np.dot(o.cov_est, rn.normal(0, 1, k)) + o.theta_est
+    def __init__(self, k, sigma=1.0, lmb=1.0):
+        self.X = []
+        self.y = []
+        self.k = k
+        self.sigma = sigma
+        self.sigmasq = sigma * sigma
+        self.lmb = lmb
+        self.theta_est = np.zeros([self.k], dtype=np.float32)
+        self.cov_est = self.sigmasq * np.eye(self.k, dtype=np.float32)
+        self.theta_sample = np.dot(self.cov_est, rn.normal(0, 1, k)) + self.theta_est
         # Tensorflow ops for computing the fit and covariance in estimate
-        o.A = tf.placeholder(tf.float32, [None, o.k])
-        o.b = tf.placeholder(tf.float32, [None])
-        o.cov = tf.matrix_inverse(mmul(tf.transpose(o.A), o.A) / o.sigmasq + o.lmb * tf.eye(o.k, o.k))
-        o.theta = mmul(o.cov, mmul(tf.transpose(o.A), tf.reshape(o.b, [-1, 1])) / o.sigmasq)
+        self.A = np.zeros((self.k, self.k), dtype=np.float32)
+        self.b = np.zeros(self.k, dtype=np.float32)
+
 
     # Store given data to be fit later
-    def add_data(o, x, yi):
-        o.X.append(x)
-        o.y.append(yi)
+    def add_data(self, x, yi):
+        self.X.append(x)
+        self.y.append(yi)
 
     # Reset stored data to start a new round of fitting
-    def reset_data(o):
-        o.X = []
-        o.y = []
+    def reset_data(self):
+        self.X = []
+        self.y = []
 
     # Perform fit
-    def fit(o, sess):
-        if len(o.X) == 0:
+    def fit(self):
+        if len(self.X) == 0:
             return
-        o.theta_est, o.cov_est = sess.run([o.theta, o.cov], feed_dict={o.A: o.X, o.b: o.y})
-        o.theta_est = o.theta_est.flatten()
+        self.cov_est = np.linalg.inv(np.matmul(self.A.T, self.A) / self.sigmasq + self.lmb * np.eye(self.k, self.k))
+        self.theta_est = np.matmul(self.cov, np.matmul(self.A.T, np.reshape(self.b, [-1, 1])) / self.sigmasq)
+        self.theta_est = self.theta_est.flatten()
 
     # Sample and store a set of parameters from the learnt Gaussian posterior
-    def sample_from_posterior(o, sess):
-        o.theta_sample = mvn_draw(sess, o.theta_est, o.cov_est)
+    def sample_from_posterior(self):
+        pass
+        # o.theta_sample = mvn_draw(sess, o.theta_est, o.cov_est)
 
     # Get regression output for given input
-    def evaluate(o, x):
-        return np.dot(o.theta_sample, x)
+    def evaluate(self, x):
+        return np.dot(self.theta_sample, x)
 
 
-class rlsvi(object):
+class rlsvi:
     # Stationary RLSVI algorithm implementation
-    def __init__(o, ns, na, sigma=1.0, lmb=1.0, gamma=0.99):
-        o.sigma = sigma
-        o.sigmasq = sigma * sigma
-        o.lmb = lmb
-        o.gam = gamma
-        o.ns = ns
-        o.na = na
+    def __init__(self, ns, na, sigma=1.0, lmb=1.0, gamma=0.99):
+        self.sigma = sigma
+        self.sigmasq = sigma * sigma
+        self.lmb = lmb
+        self.gam = gamma
+        self.ns = ns
+        self.na = na
         # One regression model for each action
-        o.models = [bayesian_regressor(ns, o.sigma, o.lmb) for i in range(o.na)]
+        self.models = [bayesian_regressor(ns, self.sigma, self.lmb) for i in range(self.na)]
 
     # Compute the estimate and reset the data stored in the models for a
     # new episode.
-    def new_episode(o, sess):
-        for i in range(o.na):
-            o.models[i].fit(sess)
-            o.models[i].sample_from_posterior(sess)
-            o.models[i].reset_data()
+    def new_episode(self):
+        for i in range(self.na):
+            self.models[i].fit()
+            self.models[i].sample_from_posterior()
+            self.models[i].reset_data()
 
     # Return the best action according to current estimates for given state
-    def choose_action(o, s):
-        return np.argmax(([o.models[i].evaluate(s) for i in range(o.na)]))
+    def choose_action(self, s):
+        return np.argmax(([self.models[i].evaluate(s) for i in range(self.na)]))
 
     # Save state transition and reward data
-    def add_data(o, s1, a, r, s2, done):
+    def add_data(self, s1, a, r, s2, done):
         # Compute the targets
         if done:
             bi = r
         else:
-            bi = r + o.gam * np.max(([o.models[i].evaluate(s2) for i in range(o.na)]))
+            bi = r + self.gam * np.max(([self.models[i].evaluate(s2) for i in range(self.na)]))
         # Store in model for the given action
-        o.models[a].add_data(s1, bi)
+        self.models[a].add_data(s1, bi)
